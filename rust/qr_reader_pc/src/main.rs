a430229ddc2fb11c38b967bc12b5863b0d8d42f7
TriplEight <TriplEight@users.noreply.github.com>
2021-09-15 14:07:48 +0000

    deploy: ec4113053be54bad60aa25664613a16a73736b71

--- rust/qr_reader_pc/src/main.rs
+++
@@ -1,9 +0,0 @@
-use qr_reader_pc::run_with_camera;
-
-fn main() {
-    match run_with_camera() {
-        Ok(line) => println!("Success! {}", line),
-        Err(e) => println!("Error. {}", e),
-    }
-}
-
